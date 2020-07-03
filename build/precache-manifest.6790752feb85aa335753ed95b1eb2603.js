49c649b36eb3923277c5e94ed4a67e199290bf74
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2020-07-04 00:47:48 +0700

    Added build

---
+++ build/precache-manifest.6790752feb85aa335753ed95b1eb2603.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a10dfeb7c73c8d4e0cefe595b20e952",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97f822e6a1785fae30c9",
+    "url": "/static/css/main.8a0ef8b9.chunk.css"
+  },
+  {
+    "revision": "72725bd0683c90089aa0",
+    "url": "/static/js/2.021b9497.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.021b9497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f822e6a1785fae30c9",
+    "url": "/static/js/main.36cd2d9c.chunk.js"
+  },
+  {
+    "revision": "fbaab9a582e8e68a6c8c",
+    "url": "/static/js/runtime-main.2ae115fb.js"
+  }
+]);
